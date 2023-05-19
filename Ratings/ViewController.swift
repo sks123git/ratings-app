d47001c012ead1b761f5ee3fdbe7e18703a438a5
Adesh Maske <adeshmaske255@gmail.com>
2023-05-19 14:32:34 +0530

    Initial submission

---
+++ Ratings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ratings
+//
+//  Created by Macbook on 18/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .darkGray
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
